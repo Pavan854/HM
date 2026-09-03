adc8cb99d43d3f03b467e4e7a008704f50e7b6d6
Pavan854 <adithyapavan854@gmail.com>
2026-09-03 09:48:54 +0530

    03-09-2026

---
+++ Desktop/java codes/plus.java
@@ -0,0 +1,16 @@
+class plus{
+    public static void main(string[]args){
+        for( int i=0;i<6;i++){
+        for(int j=0;j<6;j++){
+            if(j==3 || i==3){
+                System.out.print("*");
+            
+            
+            }
+            else{
+                System.out.println("");
+            }
+        }
+        }
+    }
+}
